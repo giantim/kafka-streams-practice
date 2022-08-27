9eab0c6e1d3f5f466c6487ecaacf94e70e409b86
imsungho <giantim@woowahan.com>
2022-08-27 13:34:31 +0900

    feat: init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kafka Streams Practice
+Kafka Streams application을 만들어보는 레포지토리
+
+- 배달 도메인을 정의해 stream application을 구현합니다.
+- Spring Cloud + Kafka Streams를 이용한 애플리케이션을 구현합니다.
---
+++ src/test/java/com/example/kafkastreamspractice/KafkaStreamsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
